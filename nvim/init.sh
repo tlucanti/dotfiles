9e86c48fdbcc07dbce57d0d350dd4431f888b0e2
Konstantin Semichastnov <konstantin.semichastnov@huawei-partners.com>
2023-08-28 17:00:19 +0800

    update init script

--- nvim/init.sh
+++ nvim/init.sh
@@ -6,6 +6,7 @@ rm -rf ~/.cache/nvim/
 mkdir ~/.config/nvim
 cp ./init.lua ~/.config/nvim/
 
-echo "INSTALL node-js npm"
+echo "INSTALL python3.10-venv"
+echo "INSTALL ripgrep"
 
 wget 'https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/AurulentSansMono.zip'
