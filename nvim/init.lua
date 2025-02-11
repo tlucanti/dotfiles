9586ad5d8d01db91bb0b8b0189213cea1d885c4d
Konstantin Semichastnov <tlucanti@yandex-team.ru>
2025-02-11 19:51:21 +0300

    nvim: add go, python language servers
    Signed-off-by: Konstantin Semichastnov <tlucanti@yandex-team.ru>

--- nvim/init.lua
+++ nvim/init.lua
@@ -175,6 +175,20 @@ local plugins = {
 	 end,
 },
 {
+	"ray-x/go.nvim",
+	dependencies = {  -- optional packages
+		"ray-x/guihua.lua",
+		"neovim/nvim-lspconfig",
+		"nvim-treesitter/nvim-treesitter",
+	},
+	config = function()
+		require("go").setup()
+	end,
+	event = {"CmdlineEnter"},
+	ft = {"go", 'gomod'},
+	build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+},
+{
         'VonHeikemen/lsp-zero.nvim',
         lazy = false,
         branch = 'v2.x',
@@ -438,10 +452,11 @@ require('lspconfig').pylsp.setup{
   settings = {
       plugins = {
           jedi = { extra_paths = {
-              '~/arc',
+              -- '~/arc',
               -- '~/arc/yt/python',
               -- '~/arc/contrib/libs/protobuf/python',
-              '~/arc/contrib/python',
+              -- '~/arc/contrib/python',
+	      '~/cloudia/cloud/compute/go/core/',
           } }
       }
   }
