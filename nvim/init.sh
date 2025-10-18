f3a742e63c49f3a01b9fa6d34d717af0e5e445de
Konstantin Semichastnov <k.semichastnov@syntacore.com>
2025-10-18 11:09:43 +0300

    update script, gitignore
    Signed-off-by: Konstantin Semichastnov <k.semichastnov@syntacore.com>

--- nvim/init.sh
+++ nvim/init.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-rm -rf ~/.config/nvim/
-rm -rf ~/.local/share/nvim/
-rm -rf ~/.local/state/nvim/
-rm -rf ~/.cache/nvim/
-
-mkdir -p ~/.config/nvim
-cp ./init.lua ~/.config/nvim/
+# rm -rf ~/.config/nvim/
+# rm -rf ~/.local/share/nvim/
+# rm -rf ~/.local/state/nvim/
+# rm -rf ~/.cache/nvim/
+#
+# mkdir -p ~/.config/nvim
+# cp ./init.lua ~/.config/nvim/
 
 if ! [ -e JetBrainsMono.zip ]; then
 	wget 'https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/JetBrainsMono.zip'
